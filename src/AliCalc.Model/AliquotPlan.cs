d8e405ad0d9d62e9f227ca08635ea96b461377d9
Scott W Harden <swharden@gmail.com>
2020-12-27 22:13:17 -0500

    create model

---
+++ src/AliCalc.Model/AliquotPlan.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AliCalc.Model
+{
+    public class AliquotPlan
+    {
+        public const double SyringeVolume_L = 10 * 1e-3;
+        public const double SyringeDilutionFactor = 50;
+
+        public readonly double TotalStockVolume_L = double.NaN;
+        public readonly double AliquotVolume_L = double.NaN;
+        public double AliquotCount => TotalStockVolume_L / AliquotVolume_L;
+
+        public readonly string Drug;
+        public readonly string Solvent;
+        public readonly double DrugMassG;
+        public readonly double DrugMW;
+        public readonly double StockM;
+        public readonly double BathM;
+
+        public AliquotPlan(string drug, double massG, double mw, string solvent, double stockM, double bathM)
+        {
+            (Drug, Solvent, DrugMassG, DrugMW, StockM, BathM) = (drug, solvent, massG, mw, stockM, bathM);
+
+            // size aliquots so 1 aliquot per 10mL syringe
+            double totalMols = massG / mw;
+            TotalStockVolume_L = totalMols / stockM;
+            double dilutionsToSyringe = stockM / (SyringeDilutionFactor * bathM);
+            AliquotVolume_L = SyringeVolume_L / dilutionsToSyringe;
+        }
+    }
+}
