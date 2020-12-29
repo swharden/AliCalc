7b486302002538a7689d1925a138438c3a6a4405
Scott W Harden <swharden@gmail.com>
2020-12-28 21:33:14 -0500

    start AliCalc Blazor project

--- src/AliCalc.Model/AliquotPlan.cs
+++ src/AliCalc.Model/AliquotPlan.cs
@@ -11,11 +11,21 @@ namespace AliCalc.Model
         public readonly Volume AliquotVolume;
         public double AliquotCount => TotalStockVolume / AliquotVolume;
 
+        public readonly string InstructionToMakeStock;
+        public readonly string InstructionToAliquotStock;
+        public readonly string InstructionSyringe;
+        public readonly string InstructionBath;
+
         public AliquotPlan(Drug drug, StockConcentration stockConcentration, BathConcentration bathConcentration)
         {
             Syringe = new Syringe(Volume.MilliLiters(10), bathConcentration, 50);
             TotalStockVolume = Volume.Liters(drug.Moles / stockConcentration.MolesPerLiter);
             AliquotVolume = Syringe.Volume / (stockConcentration / Syringe.Concentration);
+
+            InstructionToMakeStock = $"Dissolve {drug.Mass.mg} mg {drug.Name} into {TotalStockVolume.mL:0.###} mL {drug.Solvent} to make {stockConcentration.mM} mM stock.";
+            InstructionToAliquotStock = $"Divide stock into {AliquotVolume.uL:0.###} µL aliquots (makes {AliquotCount:.##}) of {stockConcentration.mM} mM {drug.Name} in {drug.Solvent}.";
+            InstructionSyringe = $"Use 1 aliquot to {Syringe.Volume.mL} mL ACSF in a {Syringe.DilutionFactor}x syringe for {bathConcentration.uM} µM bath.";
+            InstructionBath = $"Add 1 aliquot to {Syringe.Volume.mL * Syringe.DilutionFactor} mL ACSF for {bathConcentration.uM} µM bath.";
         }
     }
 }
---
+++ src/AliCalc.Web/Shared/AliquotInputModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AliCalc.Web.Shared
+{
+    public class AliquotInputModel
+    {
+        public string DrugName;
+        public bool DrugNameIsValid => !string.IsNullOrWhiteSpace(DrugName);
+    }
+}
